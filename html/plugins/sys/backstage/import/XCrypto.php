b810c39e79918f2c6f31b36ea080f82469626de0
tphp <336296@qq.com>
2022-10-21 08:58:55 +0800

    new

--- html/plugins/sys/backstage/import/XCrypto.php
+++ html/plugins/sys/backstage/import/XCrypto.php
@@ -12,9 +12,33 @@ class XCrypto
 {
     function __construct($password = '')
     {
-        $this->options = OPENSSL_RAW_DATA;
+        $this->isOpenssl = true;
+        if (defined('OPENSSL_RAW_DATA')) {
+            $this->options = OPENSSL_RAW_DATA;
+        } else {
+            if (!function_exists('mcrypt_encrypt')) {
+                // php必须包含其中一种扩展： openssl 或 mcrypt
+                throw new Exception('Either Openssl or Mcrypt must be used');
+            }
+            $this->options = 1;
+            $this->isOpenssl = false;
+        }
         empty($password) && $password = 'NUM_XCR';
         $this->password = $password;
+        $this->padPassword = $this->__getPadPassword();
+    }
+
+    /**
+     * 补齐加密字符串
+     * @return string
+     */
+    private function __getPadPassword() {
+        $passwordLen = $this->password;
+        if ($passwordLen >= 16) {
+            return substr($this->password, 0, 16);
+        }
+
+        return str_pad($this->password, 16, "\0");
     }
 
     /**
@@ -25,7 +49,19 @@ class XCrypto
      */
     private function __encrypt($input = '', $method = 'AES-128-ECB')
     {
-        $data = openssl_encrypt($input, $method, $this->password, $this->options);
+        if ($this->isOpenssl) {
+            $data = openssl_encrypt($input, $method, $this->password, $this->options);
+        } else {
+            if ($method == 'AES-128-ECB') {
+                $cipher = MCRYPT_RIJNDAEL_128;
+            } else {
+                $cipher = MCRYPT_DES;
+            }
+            $block = @mcrypt_get_block_size($cipher, "ecb");
+            $pad = $block - (strlen($input) % $block);
+            $input .= str_repeat(chr($pad), $pad);
+            $data = @mcrypt_encrypt($cipher, $this->padPassword, $input, MCRYPT_MODE_ECB);
+        }
         $data = base64_encode($data);
         return $data;
     }
@@ -38,7 +74,20 @@ class XCrypto
      */
     private function __decrypt($input = '', $method = 'AES-128-ECB')
     {
-        $data = openssl_decrypt(base64_decode($input), $method, $this->password, $this->options);
+        if ($this->isOpenssl) {
+            $data = openssl_decrypt(base64_decode($input), $method, $this->password, $this->options);
+        } else {
+            if ($method == 'AES-128-ECB') {
+                $cipher = MCRYPT_RIJNDAEL_128;
+            } else {
+                $cipher = MCRYPT_DES;
+            }
+            $input = base64_decode($input);
+            $input = @mcrypt_decrypt($cipher, $this->padPassword, $input, MCRYPT_MODE_ECB);
+            $len = strlen($input);
+            $pad = ord($input[$len - 1]);
+            $data = substr($input, 0, strlen($input) - $pad);
+        }
         if ($data === false) {
             $data = '';
         }
